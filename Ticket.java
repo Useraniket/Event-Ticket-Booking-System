12d707a26598769302ca08c8966681235c3ee40d
dead_sonu <97146375+Useraniket@users.noreply.github.com>
2025-05-14 10:29:43 +0530

    Update Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,27 +1,28 @@
-package eventTicketBooking;
+package EventTicketBookingSystem;
 
-public class Ticket {
-	
-	Attendee attendee;
-	Event event;
-	String status;
-	
-	public Ticket(Attendee attendee, Event event, String status) {
-		this.attendee = attendee;
-		this.event = event;
-		this.status = "Booked";
-	}
-	
-	public void cancel() {
-		status="Cancelled";
-	}
-	
-	public String getStatus() {
-		return status;
-	}
-	
-	public String ticketDetails() {
-		return "Attendee:" + attendee +", event:"+event+", status:"+status;
-	}
+import java.io.Serializable;
 
+public class Ticket implements Serializable {
+
+    private Attendee attendee;
+    private Event event;
+    private String status;
+
+    public Ticket(Attendee attendee, Event event) {
+        this.attendee = attendee;
+        this.event = event;
+        this.status = "Booked";
+    }
+
+    public void cancel() {
+        status = "Cancelled";
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String ticketDetails() {
+        return "Attendee: " + attendee + ", Event: " + event + ", Status: " + status;
+    }
 }
